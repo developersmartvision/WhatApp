b5c0d201be10383383c60be9846fe4174e7b2201
developer smart <developersmartvision@gmail.com>
2022-08-24 15:11:36 +0700

    global wa api

--- app/src/main/java/com/didik/whatapp/MainActivity.kt
+++ app/src/main/java/com/didik/whatapp/MainActivity.kt
@@ -13,6 +13,8 @@ import java.net.URLEncoder
 
 
 class MainActivity : AppCompatActivity() {
+    var message = " Nominal : Rp.30.000 \n Provider : IM3 \n No Pengirim : (--no pengirim) \n NoBank: 1234567 \n nama Bank : BANK BCA"
+    var phoneId = "+628815089298".replace("+", "").replace(" ", "")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,20 +27,24 @@ class MainActivity : AppCompatActivity() {
         button2.setOnClickListener {
             launchWhatsAppBusinessApp()
         }
+        var button4 = findViewById<Button>(R.id.btn4)
+        button4.setOnClickListener {
+            openWhatsappWeb()
+        }
         var button3 = findViewById<Button>(R.id.btn3)
         button3.setOnClickListener {
 
-        when {
-            isAppInstalled("com.whatsapp") -> {
-                openWhatsApp()
+            when {
+                isAppInstalled("com.whatsapp") -> {
+                    openWhatsApp()
+                }
+                isAppInstalled("com.whatsapp.w4b") -> {
+                    launchWhatsAppBusinessApp()
+                }
+                else -> {
+                    openWhatsappWeb()
+                }
             }
-            isAppInstalled("com.whatsapp.w4b") -> {
-                launchWhatsAppBusinessApp()
-            }
-            else -> {
-                openWhatsappWeb()
-            }
-        }
         }
 
     }
@@ -52,32 +58,45 @@ class MainActivity : AppCompatActivity() {
             false
         }
     }
+
     fun openWhatsApp() {
-        val pm = packageManager
+
+
         try {
-            val info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA)
-            val intent = this.packageManager.getLaunchIntentForPackage("com.whatsapp")
+            val intent = Intent("android.intent.action.MAIN")
+            intent.putExtra("jid", "$phoneId@s.whatsapp.net")
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.action = Intent.ACTION_SEND
+            intent.setPackage("com.whatsapp")
+            intent.setType("text/plain")
             startActivity(intent)
         } catch (e: PackageManager.NameNotFoundException) {
             Toast.makeText(this, "Please install WhatsApp App", Toast.LENGTH_SHORT).show()
         } catch (exception: NullPointerException) {
+            openWhatsappWeb()
         }
     }
 
     fun launchWhatsAppBusinessApp() {
-        val pm = packageManager
+
         try {
-            val info = pm.getPackageInfo("com.whatsapp.w4b", PackageManager.GET_META_DATA)
-            val intent = this.packageManager.getLaunchIntentForPackage("com.whatsapp.w4b")
+
+            val intent = Intent("android.intent.action.MAIN")
+            intent.putExtra("jid", "$phoneId@s.whatsapp.net")
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.action = Intent.ACTION_SEND
+            intent.setPackage("com.whatsapp")
+            intent.setType("text/plain")
             startActivity(intent)
         } catch (e: PackageManager.NameNotFoundException) {
             Toast.makeText(this, "Please install WA Business App", Toast.LENGTH_SHORT).show()
         } catch (exception: java.lang.NullPointerException) {
+            openWhatsappWeb()
         }
     }
 
     fun openWhatsappWeb() {
-        val message: String = "mMessOpenWhatEdit.getText().toString() "// take message from the user
+//        val message: String = "mMessOpenWhatEdit.getText().toString() "// take message from the user
 
         // create an Intent to send data to the whatsapp
         val intent = Intent(Intent.ACTION_VIEW) // setting action
@@ -85,7 +104,7 @@ class MainActivity : AppCompatActivity() {
         // setting data url, if we not catch the exception then it shows an error
         try {
             val url =
-                "https://api.whatsapp.com/send?phone=+6285706916051" + "&text=" + URLEncoder.encode(
+                "https://api.whatsapp.com/send?phone=+628815089298" + "&text=" + URLEncoder.encode(
                     message,
                     "UTF-8"
                 )
